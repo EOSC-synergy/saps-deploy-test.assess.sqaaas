105c4d17cf0d6d911205ed805ef9edbe81b3128f
orviz <orviz@ifca.unican.es>
2022-10-19 23:12:10 +0200

    Add JePL file structure

--- .sqa/script.frumpy-puce-sparrow.sh
+++ .sqa/script.frumpy-puce-sparrow.sh
@@ -4,7 +4,7 @@ cat <<EOF >> /im/auth.dat
 type = InfrastructureManager; username = %s; password = %s
 # OpenStack site using standard user, password, tenant format
 
-id = ifca; type = OpenStack; host = https://api.cloud.ifca.es:5000; username = %s; password = %s; tenant = VO:eosc-synergy.eu; tenant_domain_id = 0432c17d955e4c8d8ab7db0384536d4e; domain = IFCA; auth_version = 3.x_password
+id = incd; type = OpenStack; host = https://stratus.ncg.ingrid.pt:5000; username = %s; password = %s; tenant = eosc-synergy; domain = default; auth_version = 3.x_password
 
 EOF
 if [ -z "$IM_USER" ] || [ -z "$IM_PASS" ] || [ -z "$OPENSTACK_USER" ] || [ -z "$OPENSTACK_PASS" ]; then
@@ -19,7 +19,7 @@ echo
 mkdir -p /etc/ec3/templates
 cp -rf github.com/orviz/saps-deploy-test/ec3_recipe/* /etc/ec3/templates
 cp ec3_recipe/kubernetes-brazil.radl /etc/ec3/templates
-ec3 launch sqaaas_ec3_cluster kubernetes-brazil nfs-saps saps -a "/im/auth.dat" -u https://appsgrycap.i3m.upv.es:31443/im/ -y
+ec3 launch sqaaas_ec3_cluster kubernetes-brazil nfs-saps nfs -a "/im/auth.dat" -u https://appsgrycap.i3m.upv.es:31443/im/ -y
 ec3 show sqaaas_ec3_cluster -r
 ec3 destroy sqaaas_ec3_cluster --force -y
 )
